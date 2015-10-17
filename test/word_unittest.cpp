1ea7163f6d4b09f5ef753e4aae24a765ee45319d
Philip Pham <phil@phillypham.com>
2015-10-17 16:21:02 -0400

    updated tests for words

--- test/word_unittest.cpp
+++ test/word_unittest.cpp
@@ -24,52 +24,52 @@ TEST(tokenize, default) {
 Chyna: It should've been like any other day. Compose a four-part symphonic masterpiece, gently decline Fletcher's advances, and then walk Cameron home from school. 
 (Panting) I thought I knew who I was. I thought I knew who my friends were. But today was not like any other day. 
 (Groaning) Nothing was like it normally is. region)ZZZ";
-  // expected_phrases = std::vector<std::vector<std::string>>{std::vector<std::string>{"school", "bell", "ringing"},
-  //                                                          std::vector<std::string>{"well"},
-  //                                                          std::vector<std::string>{"least", "now", "there"},
-  //                                                          std::vector<std::string>{"plenty", "room", "for", "your", "masculinity"},
-  //                                                          std::vector<std::string>{"not", "that", "you"}, 
-  //                                                          std::vector<std::string>{"need"},
-  //                                                          std::vector<std::string>{"shake"},
-  //                                                          std::vector<std::string>{"chyna"},
-  //                                                          std::vector<std::string>{"should"},
-  //                                                          std::vector<std::string>{"been", "like", "any", "other", "day"},
-  //                                                          std::vector<std::string>{"compose", "four", "part", "symphonic", "masterpiece"},
-  //                                                          std::vector<std::string>{"gently", "decline", "fletcher"},
-  //                                                          std::vector<std::string>{"advances"},
-  //                                                          std::vector<std::string>{"and", "then", "walk", "cameron", "home", "from", "school"},
-  //                                                          std::vector<std::string>{"panting"},
-  //                                                          std::vector<std::string>{"thought", "knew", "who", "was"},
-  //                                                          std::vector<std::string>{"thought", "knew", "who", "friends", "were"},
-  //                                                          std::vector<std::string>{"but", "today", "was", "not", "like", "any", "other", "day"},
-  //                                                          std::vector<std::string>{"groaning"},
-  //                                                          std::vector<std::string>{"nothing", "was", "like", "normally"},
-  //                                                          std::vector<std::string>{"region"}};
+  expected_phrases = std::vector<std::vector<std::pair<std::string, int>>>{std::vector<std::pair<std::string, int>>{std::make_pair("school",1), std::make_pair("bell",8), std::make_pair("ringing",13)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("well",22)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("least",31), std::make_pair("now",37), std::make_pair("there",41)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("plenty",49), std::make_pair("room",59), std::make_pair("for",64), std::make_pair("your",68), std::make_pair("masculinity",73)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("not",86), std::make_pair("that",90), std::make_pair("you",95)}, 
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("need",102)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("shake",111)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("chyna",125)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("should",135)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("been",145), std::make_pair("like",150), std::make_pair("any",155), std::make_pair("other",159), std::make_pair("day",165)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("compose",170), std::make_pair("four",180), std::make_pair("part",185), std::make_pair("symphonic",190), std::make_pair("masterpiece",200)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("gently",213), std::make_pair("decline",220), std::make_pair("fletcher",228)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("advances",239)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("and",249), std::make_pair("then",253), std::make_pair("walk",258), std::make_pair("cameron",263), std::make_pair("home",271), std::make_pair("from",276), std::make_pair("school",281)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("panting",291)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("thought",302), std::make_pair("knew",312), std::make_pair("who",317), std::make_pair("was",323)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("thought",330), std::make_pair("knew",340), std::make_pair("who",345), std::make_pair("friends",352), std::make_pair("were",360)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("but",366), std::make_pair("today",370), std::make_pair("was",376), std::make_pair("not",380), std::make_pair("like",384), std::make_pair("any",389), std::make_pair("other",393), std::make_pair("day",399)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("groaning",406)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("nothing",416), std::make_pair("was",424), std::make_pair("like",428), std::make_pair("normally",436)},
+                                                                           std::vector<std::pair<std::string, int>>{std::make_pair("region",449)}};
   actual_phrases = snap::word::tokenize(text);
-  // ASSERT_EQ(expected_phrases.size(), actual_phrases.size());
-  // for (int i = 0; i < expected_phrases.size(); ++i) {
-  //   ASSERT_EQ(expected_phrases[i], actual_phrases[i]);
-  // }
+  ASSERT_EQ(expected_phrases.size(), actual_phrases.size());
+  for (int i = 0; i < expected_phrases.size(); ++i) {
+    ASSERT_EQ(expected_phrases[i], actual_phrases[i]);
+  }
 }
 
-// TEST(count_words, default) {
-//   std::vector<std::vector<std::string>> phrases{
-//     std::vector<std::string>{"philip", "chris", "person", "and"},
-//       std::vector<std::string>{"chris", "chris", "new", "word"},
-//         std::vector<std::string>{"person"},
-//           std::vector<std::string>{"no", "matches", "here"},
-//             std::vector<std::string>{"person", "and"}
-//   };
-//   std::map<std::string, int> word_counts = snap::word::count_words(phrases);
-//   ASSERT_EQ(1, word_counts["philip"]);
-//   ASSERT_EQ(3, word_counts["chris"]);
-//   ASSERT_EQ(1, word_counts["new"]);
-//   ASSERT_EQ(3, word_counts["person"]);
-//   ASSERT_EQ(2, word_counts["and"]);
-//   ASSERT_EQ(1, word_counts["no"]);
-//   ASSERT_EQ(1, word_counts["matches"]);
-//   ASSERT_EQ(1, word_counts["here"]);
-// }
+TEST(count_words, default) {
+  std::vector<std::vector<std::pair<std::string, int>>> phrases{
+    std::vector<std::pair<std::string, int>>{std::make_pair("philip",-1), std::make_pair("chris",-1), std::make_pair("person",-1), std::make_pair("and",-1)},
+      std::vector<std::pair<std::string, int>>{std::make_pair("chris",-1), std::make_pair("chris",-1), std::make_pair("new",-1), std::make_pair("word",-1)},
+        std::vector<std::pair<std::string, int>>{std::make_pair("person",-1)},
+          std::vector<std::pair<std::string, int>>{std::make_pair("no",-1), std::make_pair("matches",-1), std::make_pair("here",-1)},
+            std::vector<std::pair<std::string, int>>{std::make_pair("person",-1), std::make_pair("and",-1)}
+  };
+  std::map<std::string, int> word_counts = snap::word::count_words(phrases);
+  ASSERT_EQ(1, word_counts["philip"]);
+  ASSERT_EQ(3, word_counts["chris"]);
+  ASSERT_EQ(1, word_counts["new"]);
+  ASSERT_EQ(3, word_counts["person"]);
+  ASSERT_EQ(2, word_counts["and"]);
+  ASSERT_EQ(1, word_counts["no"]);
+  ASSERT_EQ(1, word_counts["matches"]);
+  ASSERT_EQ(1, word_counts["here"]);
+}
 
 TEST(compare_word_counts, default) {
   std::map<std::string, int> word_count_a;
